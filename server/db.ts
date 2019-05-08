979c55c069424c4ef1ef082c5b66a1fa9a2ad014
Sergey Ushakov <sergey.ushakov94@gmail.com>
2019-05-08 18:43:22 +0300

    cleaned up a little bit, created utils file

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as readline from "readline";
 import { createReadStream } from "fs";
+import { parseDependencies, KEY_VALUE_DIVIDER } from "./utils";
 
 enum RequiredDescriptors {
   Package = "Package",
@@ -24,6 +25,13 @@ const packageData: PackageData = {};
 
 const pathToFile = path.join(__dirname, "../", "mockdata");
 
+const reportMemoryUsage = () => {
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(
+    `The script uses approximately ${Math.round(used * 100) / 100} MB`
+  );
+};
+
 const initialize = async () => {
   const fileStream = createReadStream(pathToFile);
   const readLineInterface = readline.createInterface({
@@ -32,82 +40,58 @@ const initialize = async () => {
 
   parseData(readLineInterface);
 
-  readLineInterface.on("close", () => {
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(
-      `The script uses approximately ${Math.round(used * 100) / 100} MB`
-    );
-  });
+  readLineInterface.on("close", reportMemoryUsage);
 };
+
+let currentPackage = "";
+let prevKey = "";
+let prevValue = "";
 
 const parseData = (readLineInterface: readline.Interface) => {
-  let currentPackage = "";
-  let prevKey = "";
-  let prevValue = "";
-
-  readLineInterface.on("line", line => {
-    if (line === "") {
-      currentPackage = "";
-      return;
-    }
-
-    const isNewKey = !line.startsWith(" ");
-
-    if (isNewKey) {
-      const colonLocation = line.indexOf(": ");
-
-      const key = line.slice(0, colonLocation);
-      const value = line.substring(colonLocation + 2);
-
-      switch (prevKey) {
-        case RequiredDescriptors.Package:
-          currentPackage = prevValue;
-          packageData[currentPackage] = {};
-          break;
-        case RequiredDescriptors.Depends:
-          packageData[currentPackage] = {
-            ...packageData[currentPackage],
-            dependencies: parseDependencies(prevValue)
-          };
-          break;
-        case RequiredDescriptors.Version:
-          packageData[currentPackage] = {
-            ...packageData[currentPackage],
-            version: prevValue
-          };
-          break;
-        case RequiredDescriptors.Description:
-          packageData[currentPackage] = {
-            ...packageData.currentPackage,
-            description: prevValue
-          };
-        default:
-          break;
-      }
-
-      prevKey = key;
-      prevValue = value;
-    } else {
-      prevValue += line.substring(1);
-    }
-  });
-
-  readLineInterface.on("close", () => console.log(packageData));
+  readLineInterface.on("line", lineParser);
 };
 
-const parseDependencies = (line: string) => {
-  const dependencies = line.split(", ").map(removeVersionFromDependency);
+const lineParser = line => {
+  if (line === "") {
+    currentPackage = "";
+    return;
+  }
 
-  return new Set(dependencies);
-};
+  const isNewKey = !line.startsWith(" ");
 
-const removeVersionFromDependency = (dependency: string) => {
-  const [dependencyName] = dependency.split(" ");
+  if (isNewKey) {
+    const [key, value] = line.split(KEY_VALUE_DIVIDER);
 
-  if (dependencyName) {
-    return dependencyName.trim();
+    switch (prevKey) {
+      case RequiredDescriptors.Package:
+        currentPackage = prevValue;
+        packageData[currentPackage] = {};
+        break;
+      case RequiredDescriptors.Depends:
+        packageData[currentPackage] = {
+          ...packageData[currentPackage],
+          dependencies: parseDependencies(prevValue)
+        };
+        break;
+      case RequiredDescriptors.Version:
+        packageData[currentPackage] = {
+          ...packageData[currentPackage],
+          version: prevValue
+        };
+        break;
+      case RequiredDescriptors.Description:
+        packageData[currentPackage] = {
+          ...packageData.currentPackage,
+          description: prevValue
+        };
+      default:
+        break;
+    }
+
+    prevKey = key;
+    prevValue = value;
   } else {
-    return null;
+    prevValue += line.substring(1);
   }
 };
 
