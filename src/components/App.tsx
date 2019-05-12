2b2f648a5d50c38898a4787a0121befd61977ca3
Sergey Ushakov <sergey.ushakov94@gmail.com>
2019-05-12 12:28:55 +0300

    added css resets

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import PackageList from "./PackageList";
 import PackageDetails from "./PackageDetails";
 
+import "normalize.css";
 import "../styles/App.css";
 
 const App: React.FC = () => {
