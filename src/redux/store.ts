5329db05aebe7b508708ebd2134d43298f19d7d9
Sergey Ushakov <sergey.ushakov94@gmail.com>
2019-05-15 20:16:34 +0300

    fixed crash on browsers without redux devtools extensions

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,12 +7,8 @@ const reducers = combineReducers({
   packageList
 });
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
